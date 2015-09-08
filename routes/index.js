2b1c91bb7ebdb0825cfee206257123413ca667ca
Sanjeev Suresh <sanjeevsuresh95@gmail.com>
2015-09-07 23:57:34 -0700

    email me if something goes wrong

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,33 @@
 var express = require('express');
+var nodemailer = require('nodemailer');
 var router = express.Router();
+var transporter = nodemailer.createTransport({
+	service: 'Gmail',
+    auth: {
+        user: 'sanjeevsuresh.online@gmail.com', // Your email id
+        pass: 'REPLACE_ME' // Your password
+    }
+});
+var text = 'A transaction failed';
+var mailOptions = {
+    from: 'BIG <sanjeevsuresh.online@gmail.com>', // sender address
+    to: 'sanjeevsuresh95@gmail.com', // list of receivers
+    subject: 'Failed Transaction', // Subject line
+    text: text //, // plaintext body
+    // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/join', function(req, res, next) {
+  res.redirect('pay.html');
+});
+router.get('/apply', function(req, res, next) {
+  res.redirect('getInvolved.html#InternProgram');
+});
+router.get('')
 router.post('/charge', function (request, response, next) {
 	// Set your secret key: remember to change this to your live secret key in production
 	// See your keys here https://dashboard.stripe.com/account/apikeys
@@ -22,7 +45,14 @@ router.post('/charge', function (request, response, next) {
 	}, function(err, charge) {
 	  if (err && err.type === 'StripeCardError') {
 	    // The card has been declined
-	    response.send('Your transaction did not go through for the following reason:\n' + err.message);
+		transporter.sendMail(mailOptions, function(error, info){
+		    if(error){
+		        console.log(error);
+		    }else{
+		        console.log('Message sent: ' + info.response);
+		    };
+		});
+	    response.redirect('decline.html');
 	    console.log(err);
 	  } else {
 	  	response.redirect('https://docs.google.com/forms/d/1m9In9HW78jVlUPoqtgnJ_ymosLhlcg9FgBXx_bOTXOw/viewform');
